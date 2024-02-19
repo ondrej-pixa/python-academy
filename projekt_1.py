5a097325c2070597f369f4ecb98b8ba48385572e
Ondrej Pixa <ondrej.pixa@gmail.com>
2024-02-19 21:02:35 +0100

    pocitani slov a cisel

--- projekt_1.py
+++ projekt_1.py
@@ -5,6 +5,34 @@ email: ondrej.pixa@gmail.com
 discord: ondra_32
 """
 
+TEXTS = ['''
+Situated about 10 miles west of Kemmerer,
+Fossil Butte is a ruggedly impressive
+topographic feature that rises sharply
+some 1000 feet above Twin Creek Valley
+to an elevation of more than 7500 feet
+above sea level. The butte is located just
+north of US 30N and the Union Pacific Railroad,
+which traverse the valley. ''',
+'''At the base of Fossil Butte are the bright
+red, purple, yellow and gray beds of the Wasatch
+Formation. Eroded portions of these horizontal
+beds slope gradually upward from the valley floor
+and steepen abruptly. Overlying them and extending
+to the top of the butte are the much steeper
+buff-to-white beds of the Green River Formation,
+which are about 300 feet thick.''',
+'''The monument contains 8198 acres and protects
+a portion of the largest deposit of freshwater fish
+fossils in the world. The richest fossil fish deposits
+are found in multiple limestone layers, which lie some
+100 feet below the top of the butte. The fossils
+represent several varieties of perch, as well as
+other freshwater genera and herring similar to those
+in modern oceans. Other fish such as paddlefish,
+garpike and stingray are also present.'''
+]
+
 users = {
     "bob": "123",
     "ann": "pass123",
@@ -42,3 +70,31 @@ if not (1 <= number <= 3):
     exit()
 
 print("----------------------------------------")
+
+text = TEXTS[number - 1]
+words = text.split()
+
+words_count = len(words)
+titlecase_words_count = 0
+uppercase_words_count = 0
+lowercase_words_count = 0
+numeric_strings_count = 0
+sum_of_numbers = 0
+
+for word in words:
+    if word.istitle():
+        titlecase_words_count += 1
+    if word.isupper() and word.isalpha():
+        uppercase_words_count += 1
+    if word.islower():
+        lowercase_words_count += 1
+    if word.isdigit():
+        numeric_strings_count += 1
+        sum_of_numbers += int(word)
+
+print(f"There are {words_count} words in the selected text.")
+print(f"There are {titlecase_words_count} titlecase words.")
+print(f"There are {uppercase_words_count} uppercase words.")
+print(f"There are {lowercase_words_count} lowercase words.")
+print(f"There are {numeric_strings_count} numeric strings.")
+print(f"The sum of all the numbers {sum_of_numbers}")
